4330fbdb301d79eea71d46ba1ad4dc0e2e5177e6
Pree <preeyashp@gmail.com>
2020-07-31 15:57:36 +1000

    fixed js file swaps

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,8 +6,8 @@ $(window).scroll(function () {
     }
     else {
         $('#grey-box').removeClass('full-opacity');
-    }
-})
+    };
+});
 $(window).scroll(function () {
     var eh = $('header').height();
     var dh = $('#designer').height();
@@ -20,12 +20,12 @@ $(window).scroll(function () {
 
     if ($(this).scrollTop() > (30 / 100) * eh && $(this).scrollTop() < ($('#designer').offset().top + (30 / 100) * dh)) {
         nav.addClass('red-bg');
-        red.addClass('full-opacity')
+        red.addClass('full-opacity');
     }
     else {
         nav.removeClass('red-bg');
         red.removeClass('full-opacity');
-    }
+    };
     // ($(this).scrollTop() > odv)?nav.addClass('blue-bg'):nav.removeClass('blue-bg')
     // ($(this).scrollTop() > op)?nav.addClass('orange-bg'):nav.removeClass('orange-bg')
     // ($(this).scrollTop() > oc)?nav.addClass('green-bg'):nav.removeClass('green-bg') 
@@ -34,7 +34,7 @@ $(window).scroll(function () {
     //    $("#dv-item").fadeIn("slow"); 
     //  }
 
-})
+});
 
 $(window).scroll(function () {
     var nav = $('nav');
@@ -48,8 +48,8 @@ $(window).scroll(function () {
     else {
         nav.removeClass('blue-bg');
         blue.removeClass('full-opacity');
-    }
-})
+    };
+});
 
 $(window).scroll(function () {
     var nav = $('nav');
@@ -63,8 +63,8 @@ $(window).scroll(function () {
     else {
         nav.removeClass('orange-bg');
         orange.removeClass('full-opacity');
-    }
-})
+    };
+});
 
 $(window).scroll(function () {
     var nav = $('nav');
@@ -78,118 +78,28 @@ $(window).scroll(function () {
     else {
         nav.removeClass('green-bg');
         green.removeClass('full-opacity');
-    }
-})
+    };
+});
 
 $(window).scroll(function () {
     var ui = $('#ui-ux-button');
     if ($(this).scrollTop() > ($("#designer").offset().top + 50)) {
         ui.addClass("animated fadeIn");
-    }
-})
+    };
+});
 
 $(window).scroll(function () {
     var dv = $('#dv-item');
     if ($(this).scrollTop() > ($("#designer").offset().top + 200)) {
         dv.addClass("animated fadeIn");
-    }
-})
+    };
+});
 
 
 $(window).scroll(function () {
     var dv = $('#dev-cont');
     if ($(this).scrollTop() > ($("#designer").offset().top + 300)) {
         dv.addClass("animated fadeIn");
-    }
-})
-
-$(".toggle-icon").click(function () {
-    $('nav').toggleClass("pushed");
-    $('#sideNav').toggleClass("pushSide")
-    $("#sideNav-content").fadeToggle("slow");
-    $("#smallNav").slideToggle(200);
-    $('body').toggleClass("stop-scroll");
+    };
 });
 
-setTimeout(function () {
-    $('.line').addClass("display-line")
-
-}, 800)
-
-$('#animate-head').addClass("animated fadeIn")
-$('#hi').addClass("animated bounceIn")
-
-
-
-let getTo = () => {
-    $('nav').removeClass("pushed");
-    $('#sideNav').removeClass("pushSide");
-    $('#sideNav-content').fadeOut();
-    $('#smallNav').slideUp(200);
-    $('body').removeClass("stop-scroll");
-}
-
-
-// uncomment after wards
-// $('html, body').animate({
-//   scrollTop:$("header").offset().top
-// },1)
-
-$("#develop").click(function () {
-    getTo();
-    $('html,body').animate({
-        scrollTop: $("#developer").offset().top - 30
-    }, 800)
-})
-
-$("#design").click(function () {
-    getTo();
-    $('html,body').animate({
-        scrollTop: $("#designer").offset().top - 30
-    }, 800)
-})
-
-$("#project").click(function () {
-    getTo();
-    $('html,body').animate({
-        scrollTop: $("#projects").offset().top - 30
-    }, 800)
-})
-
-$("#contact").click(function () {
-    getTo();
-    $('html,body').animate({
-        scrollTop: $("#contacts").offset().top - 30
-    }, 800)
-})
-
-
-$("#grey-box").click(function () {
-    $('html, body').animate({
-        scrollTop: $("header").offset().top
-    }, 800)
-})
-
-$("#red-box").click(function () {
-    $('html,body').animate({
-        scrollTop: $("#designer").offset().top - 30
-    }, 800)
-})
-
-$("#blue-box").click(function () {
-    $('html,body').animate({
-        scrollTop: $("#developer").offset().top - 30
-    }, 800)
-})
-
-$("#orange-box").click(function () {
-    $('html,body').animate({
-        scrollTop: $("#projects").offset().top - 30
-    }, 800)
-})
-
-$("#green-box").click(function () {
-    $('html,body').animate({
-        scrollTop: $("#contacts").offset().top - 30
-    }, 800)
-})
