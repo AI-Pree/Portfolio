39e35d8bf980d45369c779a771e577f1cebf1e1e
ai-pree <sexypistolcs@gmail.com>
2021-08-16 22:22:45 +1000

    making website a bit pretty

--- src/components/projcets.tsx
+++ src/components/projcets.tsx
@@ -5,11 +5,31 @@ export default function Projects(): ReactElement {
     return (
         <section className="projects">
             <h2>
-                <span className="orange-color">Projects</span>
+                <span className="green-color">Projects</span>
             </h2>
-            {projects.map((project) => (
-                <p key={project.id}>{project.title}</p>
-            ))}
+            <ul>
+                {projects.map((project) => (
+                    <li key={project.id}>
+                        <ul className="display-inline">
+                            <li>
+                                <h2 className="grey-trans">{project.title}</h2>
+                            </li>
+                            <li>
+                                <h1 className="green-color">
+                                    {project.spent}
+                                    <span className="day-font grey-trans">days</span>
+                                </h1>
+                            </li>
+                        </ul>
+                        <p>
+                            <span className="light-bold green-color">
+                                {project.description}
+                            </span>
+                        </p>
+                        <p>{project.details}</p>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
